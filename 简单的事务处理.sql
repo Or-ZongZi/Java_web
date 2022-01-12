19646aca1b4a692fbf2729a280339be91403f031
宋捷 <90044299+Or-ZongZi@users.noreply.github.com>
2022-01-12 11:39:30 +0800

    Add files via upload

---
+++ 简单的事务处理.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.57 : Database - newsdb_6
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`newsdb_6` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `newsdb_6`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accountNo` varchar(25) NOT NULL,
+  `balance` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+insert  into `account`(`id`,`accountNo`,`balance`) values (1,'张三','30000.99'),(2,'李四','29000.88'),(3,'王五','31000.99');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
